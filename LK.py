89a0079cd916f5178ee1a0a1be62d94d81caeb60
DillonHanlon <dfh031@mun.ca>
2020-08-03 10:47:50 -0230

    file for lightcurve data

---
+++ LK.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import exoplanet as xo
+import lightkurve as lk
+from lightkurve import search_lightcurvefile
+
+
+input = "KIC 8264492"
+mission = "Kepler"
+label = input
+quarter = 1
+LC = lk.search_lightcurvefile(input, mission=mission).download_all().PDCSAP_FLUX.stitch().remove_nans()
+
+tpf = lk.search_targetpixelfile(input).download()
+
+
