fa9dcbd34062310719c4934699ed7b3566f62835
DillonHanlon <dfh031@mun.ca>
2020-08-03 19:31:31 -0230

    added a savefig command

--- Maelstrom_Frequency.py
+++ Maelstrom_Frequency.py
@@ -34,23 +34,27 @@ opt = ms.optimize()
 td_time, td_td = ms.get_time_delay()
 td_average = np.average(td_td, axis=-1, weights=ms.get_weights())
 
-# plt.figure(figsize=(8,8))
-# with ms:
-#     model_td = xo.eval_in_model(ms.tau, opt) * 86400
-#     plt.plot(ms.time, model_td - np.median(model_td), c='blue', linewidth=0.5)
-#     plt.plot(td_time, td_average, '.k')
+plt.figure(figsize=(8,8))
+with ms:
+    model_td = xo.eval_in_model(ms.tau, opt) * 86400
+    plt.plot(ms.time, model_td - np.median(model_td), c='blue', linewidth=0.5)
+    plt.plot(td_time, td_average, '.k')
+    plt.savefig('time_delay.pdf')
     
-# plt.xlabel('Time [day]')
-# plt.ylabel('Time delay [s]')
+plt.xlabel('Time [day]')
+plt.ylabel('Time delay [s]')
 
-# plt.figure(figsize=(8,8))
-# with ms:
-#     plt.plot(ms.time, xo.eval_in_model(ms.lc_model, opt), c='blue', linewidth=0.5, label='Maelstrom')
-#     plt.plot(ms.time, ms.flux, '.k', label='Data')
+plt.figure(figsize=(8,8))
+with ms:
+    plt.plot(ms.time, xo.eval_in_model(ms.lc_model, opt), c='blue', linewidth=0.5, label='Maelstrom')
+    plt.plot(ms.time, ms.flux, '.k', label='Data')
+    plt.xlim(200,205)
+    plt.ylim(-6,6)
+    plt.savefig('lightcurve_opt.pdf')
 # plt.xlim(200,205)
-# plt.xlabel('Time [day]')
-# plt.ylabel('Flux [ppt]')
-# plt.legend()
+plt.xlabel('Time [day]')
+plt.ylabel('Flux [ppt]')
+plt.legend()
 
 
 # eccentricity = opt['eccen']
