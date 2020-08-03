74f1de3b54d631cdc77c7114998976bce443dc32
DillonHanlon <dfh031@mun.ca>
2020-08-03 10:48:18 -0230

    Recover star(or planet) pixel image

---
+++ Recover_Planet.py
@@ -0,0 +1,12 @@
+import lightkurve as lk
+import numpy as np
+import matplotlib.pyplot as plt
+
+from LK import tpf
+from LK import label
+
+tpf.plot(frame=100, scale='log', show_colorbar=True)
+plt.savefig('Recover_Binary.pdf')
+
+
+plt.show()
