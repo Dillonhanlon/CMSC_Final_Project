137818fbf6370a9bc52538627a0a824f019eec6e
DillonHanlon <dfh031@mun.ca>
2020-08-04 16:46:58 -0230

    fixed a problem with figure

--- orbit.py
+++ orbit.py
@@ -13,11 +13,12 @@ v=0    #y-position of the center
 u1 = a
 
 t = np.linspace(0, 2*pi, 100)
-plt.plot(u+a*np.cos(t) , v+b*np.sin(t) )
-plt.plot(rmean , 0, 'ro', markersize=14)
+fig, ax = plt.subplots(figsize = (10,10))
+plt.plot(u+a*np.cos(t) , v+b*np.sin(t), label = 'Binary star orbit' )
+plt.plot(rmean , 0, 'ro', markersize=14, label = 'Barycenter')
 ax = plt.gca()
 ax.axes.xaxis.set_visible(False)
 ax.axes.yaxis.set_visible(False)
-ax.axhline(c='grey',linestyle = '--', lw=1)
-
+ax.axhline(c='grey',linestyle = '--', lw=1,label = 'Equitorial Plane')
+ax.legend()
 plt.savefig('orbit.png')
