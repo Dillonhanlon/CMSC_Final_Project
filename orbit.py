abb4db9c77d34873a5d197f09a1265a94f67099f
DillonHanlon <dfh031@mun.ca>
2020-08-04 11:41:33 -0230

    code to output simple orbit

---
+++ orbit.py
@@ -0,0 +1,24 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from math import pi
+
+from Maelstrom_Frequency import opt
+eccen = np.mean(opt['eccen'])
+a = np.mean(opt['lighttime'])
+b = a / 2
+rmean = (a * (eccen))
+
+u=0     #x-position of the center
+v=0    #y-position of the center
+u1 = a
+
+t = np.linspace(0, 2*pi, 100)
+plt.plot(u+a*np.cos(t) , v+b*np.sin(t) )
+plt.plot(rmean , 0, 'ro', markersize=14)
+ax = plt.gca()
+ax.axes.xaxis.set_visible(False)
+ax.axes.yaxis.set_visible(False)
+ax.axhline(c='grey',linestyle = '--', lw=1)
+
+plt.savefig('orbit.pdf')
+plt.show()
