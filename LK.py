2389f9bf5f928217d2b7be2d849cdfee81b4b53b
DillonHanlon <dfh031@mun.ca>
2020-08-05 14:20:43 -0230

    Added title to figure

--- LK.py
+++ LK.py
@@ -3,6 +3,11 @@ import matplotlib.pyplot as plt
 import exoplanet as xo
 import lightkurve as lk
 from lightkurve import search_lightcurvefile
+import matplotlib as mpl
+from matplotlib import rcParams
+mpl.rcParams['axes.linewidth'] = 1.2
+rcParams["figure.dpi"] = 150
+rcParams["savefig.dpi"] = 150
 
 
 input = "KIC 8264492"
@@ -12,8 +17,10 @@ quarter = 1
 LC = lk.search_lightcurvefile(input, mission=mission).download_all().PDCSAP_FLUX.stitch().remove_nans()
 
 tpf = lk.search_targetpixelfile(input).download()
+plt.figure(figsize = (8,8))
 
 LC.plot()
+plt.title('Normalized Flux vs Time [BKJD days]')
 plt.savefig('Lightcurve.png')
 
 
