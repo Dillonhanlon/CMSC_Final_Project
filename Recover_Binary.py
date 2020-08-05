54c9d9e77739712eeb13a76feb5210eafd1475ab
DillonHanlon <dfh031@mun.ca>
2020-08-05 18:48:10 -0230

    Updated image style command

--- Recover_Binary.py
+++ Recover_Binary.py
@@ -8,8 +8,10 @@ mpl.rcParams['axes.linewidth'] = 1.2
 from LK import tpf
 from LK import label
 
-tpf.plot(frame=100, scale='log', show_colorbar=True)
+
+x = tpf.plot(frame=100, scale='log', show_colorbar=True,style = 'fast')
 plt.title('KIC 8264492',fontsize = 14)
+
 plt.xlabel('Pixel Column Number [No Units]')
 plt.ylabel('Pixel Row Number [No Units]')
 plt.savefig('Recover_Binary.png')
